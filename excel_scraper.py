c9cfc12d37c798177be7c373778e8c4b181dac7c
s-bhatla <sidroot02@gmail.com>
2023-04-01 22:31:21 +0530

    removed comments from scraper.py

--- excel_scraper.py
+++ excel_scraper.py
@@ -5,28 +5,13 @@ with open('body.txt', 'r') as file:
     # Read the contents of the file into a string variable
     body = file.read()
 
-
-# print(df)
-# print("+++++++++++++++++++++++")
-# print(len(df.index))
-# print("+++++++++++++++++++++++")
-# print(df["company_name"][0])
-
-# LocalBody = body.split(" ")
-# localstring = ""
-# print("================")
-
-
 def punctsreplace(bodyArr, arr, it, i):
     flag = 0
     puncts = [".", ",", "/", "-", ":", ";", "?", "!", ""]
 
     for j in puncts:
         for k in arr:
-            # print(bodyArr[i] + " and " + k+j)
             if (bodyArr[i] == k+j):
-                # print(str(df[k][it]) + "yoyo\n")
-                # print("here")
                 bodyArr[i] = str(df[k][it])+j
                 flag = 1
                 break
@@ -59,34 +44,3 @@ def replaceWords(body, arr, it):
         localstring += i
         localstring += " "
     return localstring
-
-
-# cols = ["company_name", "company_mail", "contact_name", "contact_number"]
-
-# print(replaceWords(body, cols, 0))
-
-
-# for i in range(len(LocalBody)):
-#     if("\n" in LocalBody[i]):
-#         newlinesep = LocalBody[i].split("\n")
-#         for j in range(len(newlinesep)):
-#             if(newlinesep[j] == "company_name"):
-#                 newlinesep[j] = df["company_name"][0]
-#                 print("what")
-#             if(newlinesep[j] == "company_name,"):
-#                 newlinesep[j] = df["company_name"][0]+","
-#                 print("what")
-#             if(newlinesep[j] == "company_name."):
-#                 newlinesep[j] = df["company_name"][0] + "."
-#                 print("what")
-#         concatlinesep = ""
-#         for k in newlinesep:
-#             concatlinesep += k
-#             concatlinesep += "\n"
-#         concatlinesep = concatlinesep[:-1]
-#         LocalBody[i] = concatlinesep
-
-# for i in LocalBody:
-#     localstring += i
-#     localstring += " "
-# print(localstring)
